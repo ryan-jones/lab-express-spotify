50ec01460906ee88826144cb1c48eb74914f01f3
ryan-jones <ryanllewellynjones@gmail.com>
2017-04-26 18:11:52 +0200

    added search input

--- express-spotify-app/app.js
+++ express-spotify-app/app.js
@@ -1,9 +1,35 @@
 const SpotifyWebApi   = require('spotify-web-api-node');
 const spotify         = new SpotifyWebApi();
+const express         = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser      =require('body-parser');
+const morgan          =require('morgan');
 
-spotify.searchArtists("The Beatles", {}, (err, data) => {
-  if (err) throw err;
+let app = express();
 
-  let artists = data.body.artists.items;
-  console.log(artists)
+app.use(express.static('public'));
+// app.use(expressLayouts);
+// app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( morgan(`dev`));
+
+
+app.get('/', (req, res, next) => {
+  res.render('home');
+});
+
+app.post('/artists', (req, res, next) => {
+  spotify.searchArtists(req.body.name, {}, (err, data) => {
+    if (err) throw err;
+
+    let artists = data.body.artists.items;
+    console.log(artists);
+    // Render after the data from spotify has returned
+    res.render('artists', {artists});
+  });
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
 });
